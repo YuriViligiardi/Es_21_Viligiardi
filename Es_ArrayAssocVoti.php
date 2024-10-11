3fd6a5b99f46fc5f4d5c47622af9abd951a158b2
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-11 08:42:03 +0200

    creato Array associativo student e la funzione per stampare i dati con la list

--- Es_ArrayAssocVoti.php
+++ Es_ArrayAssocVoti.php
@@ -7,6 +7,29 @@
 </head>
 <body>
     <?php
+        $cognome = "Viligiardi";
+        $nome = "Yuri";
+        $listVoti = array(rand(2,10), rand(2,10), rand(2,10), rand(2,10));
+        $student = array("cognome" => $cognome, "nome" => $nome, "listVoti" => $listVoti);
+        createList($student);
+
+        function createList($s){
+            $media = 0;
+            echo "<ul>";
+                echo "<li>$s[cognome]</li>"; 
+                echo "<li>$s[nome]</li>";
+                echo "<li>Lista voti:";
+                    echo "<ol>";
+                        foreach ($s["listVoti"] as $key) {
+                            echo "<li>$key</li>";
+                            $media = $media + $key;
+                        }
+                    echo "</ol>";
+                echo "</li>";
+            echo "</ul>";
+            $media = $media / count($s["listVoti"]);
+            echo "<span>La media dei voti è $media</span>";
+        }
     ?>
 </body>
 </html>
